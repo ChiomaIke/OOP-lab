086c9df53d4acb593def01ccec8d4c8ba58b7a10
Chioma Ike <nwigwechioma@gmail.com>
2016-11-29 02:28:28 +0000

    updated app folder

--- app/library.js
+++ app/library.js
@@ -1,18 +1,37 @@
 module.exports = {
-car: function(name, model, category){
-
-           this.name: "",
-           this.model: "",
-           this.category: "",
-           this.numOfDoors: 4,
-           this.numOfWheels: 4,
-           this.speed: 0,
+car: function(name, model, category, passengers, color, mileage, year){
+	    
+			this.name = function () { 
+				if (this.name === null) {
+					this.name = 'General';
+				}
+				if (this.name === string) {
+				  this.name = name;
+				}
+			};
+			this.category = category;
+			this.model = function () {
+				this.model = model;
+				return model};
+			this.color = color;
+			this.mileage = function() {
+				
+			this.passengers = passengers;
+			this.year = year;
+			this.started = false;
+			this.start = function() {
+				this. started = true;
+			};
+			this.stop = function() {
+				this.started = false;
+			};
+			this.drive = function() {
+				if (this.started) {
+				}
+			};
+			
            
-           drive: function (value) {
-                this.speed = value;
-                return this;
-           }
-
+           
            if (typeof name === 'string') {
                 this.name = name;
             }
@@ -37,5 +56,10 @@ car: function(name, model, category){
                 }
             }
 
-    }
+
+};
+
+
+
+}
 }
